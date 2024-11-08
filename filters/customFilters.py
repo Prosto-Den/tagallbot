103d68686813744625556a6243c158f33b0a581b
grishanya <grishatavro@gmail.com>
2024-11-08 17:43:40 +0300

    make bot repeat after everyone

--- filters/customFilters.py
+++ filters/customFilters.py
@@ -1,6 +1,23 @@
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from bot import bot
+from dataclasses import dataclass
+
+class SupportMessage:
+    @dataclass(slots=True)
+    class MessageState:
+        repeated_message: str = None
+        sent_message: str = None
+    
+    __chats: dict[int, MessageState] = {}
+
+    @staticmethod
+    def get(chat_id: int) -> MessageState:
+        if __chats.get(chat_id):
+            return __chats[chat_id]
+        __chats[chat_id] = SupportMessage.MessageState()
+        return __chats[chat_id]
+
 
 
 class CustomFilters:
@@ -18,3 +35,11 @@ class CustomFilters:
     async def is_any_state(_, state: FSMContext) -> bool:
         current_state = await state.get_state()
         return current_state is not None
+
+    @staticmethod
+    async def is_repeated(message: Message) -> bool:
+        messages = SupportMessage.get(message.chat.id)
+        if messages.repeated_message == message.text and messages.sent_message != message.text:
+            return True
+        messages.repeated_message = message.text
+        return False
