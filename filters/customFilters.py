95be0b00d5e87e34ca6717e9e147a9c10a507421
Prosto-Den <122309719+Prosto-Den@users.noreply.github.com>
2024-11-19 21:42:29 +0300

    HOTFIX

--- filters/customFilters.py
+++ filters/customFilters.py
@@ -63,7 +63,7 @@ class CustomFilters:
 
     @staticmethod
     async def is_prekl(message: Message) -> bool:
-        match = re.match(r'[\w\W]*(ок|да|нет)[., ?!]*$', message.text.lower())
+        match = re.match(r'\b(ок|да|нет)[., ?!]*$', message.text.lower())
 
         if match:
             bot.prekl_msg[message.chat.id] = match.group(1)
