8f4294bb5b61d536360f2d272b34d2d7ade22637
Prosto-Den <122309719+Prosto-Den@users.noreply.github.com>
2025-12-16 20:25:21 +0300

    Logger update (#18)
    * Убрал лишнее из настроек;
    * Добавил комментарии к path_helper.py
    
    * Добавил файл .yaml для CI/CD
    
    * Обновил логгер
    
    * Обновил requirements.txt

--- start_bot.py
+++ start_bot.py
@@ -2,11 +2,26 @@ from bot import dp, bot
 from handlers import messages_router
 from aiogram.methods import DeleteWebhook
 import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 
-async def main():
+#TODO пока задач немного пойдёт и так. Но если их будет много, стоит создать отдельную функцию (или даже класс)
+async def main() -> None:
+    scheduler = AsyncIOScheduler() # для создания задач, исполняющихся по расписанию
+
+    bot.get_logger().info('Создаю задачу на обновление файла логов')
+    scheduler.add_job(bot.get_logger().update_file_handler,
+                      trigger=CronTrigger(hour=0, minute=0),
+                      id='logger_date_update',
+                      replace_existing=True)
+    bot.get_logger().info('Задача создана!')
+
     await bot(DeleteWebhook(drop_pending_updates = True))
     dp.include_routers(messages_router)
+    scheduler.start()
+    bot.get_logger().info('Запущен планировщик')
+
     await dp.start_polling(bot)
 
 
